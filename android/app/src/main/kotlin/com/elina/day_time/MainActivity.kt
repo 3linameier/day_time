34d51b8d6e01f5a13e5175028e97e59b39e1bdcc
Elina Meier <3linameier@gmail.com>
2021-05-03 18:53:49 +0300

    bunch of changes

---
+++ android/app/src/main/kotlin/com/elina/day_time/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.elina.day_time
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,27 @@
+///
+/// Autor: Elina Meier
+///
+/// CGI päeva pikkuse leidmise rakendus
+///
+/// mai 2021
+///
+
 import 'package:flutter/material.dart';
+import 'package:sunrise_sunset/sunrise_sunset.dart';
+import 'package:duration/duration.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // Rakenduse juur
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'len(päev)',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+        primarySwatch: Colors.amber,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Päeva pikkuse leidja'),
@@ -34,84 +32,186 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+// Siin on erinevad meetodid ja rakenduse muutumised
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  DateTime hetkeKuupaev = DateTime.now();
+  final latController = TextEditingController();
+  final longController = TextEditingController();
+  double lat = 0;
+  double long = 0;
+  String paiksetous = "";
+  String paikseloojang = "";
+  String paevapikkus = "";
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+
+  /// Meetod leiab SunriseSunset API-st vastavalt kuupäevale ja
+  /// lokatsioonile päiksetõusu ja -loojangu aja ning päeva pikkuse
+  Future<void> _leiaPaevaPikkus() async {
+    try {
+      final response = await SunriseSunset.getResults(date: hetkeKuupaev ,latitude: lat, longitude: long);
+
+      if (response.success) {
+        DateTime dataSR = response.data.sunrise.toLocal();
+        DateTime dataSS = response.data.sunset.toLocal();
+        Duration dayLen = dataSS.difference(dataSR);
+        setState(() {
+          paiksetous = '${dataSR.hour}:${dataSR.minute}:${dataSR.second}';
+          paikseloojang = '${dataSS.hour}:${dataSS.minute}:${dataSS.second}';
+          paevapikkus = prettyDuration(dayLen, abbreviated: true);
+        });
+      } else setState(() {paiksetous = response.error;});
+    } catch (err) {
+      setState(() {paiksetous = "Failed to get data."+err;});
+    }
   }
 
+   /// meetod kontrollib, kas kasutaja sisestatud koordinaadid sobivad
+   String _kontrollKoordinaate() {
+    try {
+      lat = double.parse(latController.text);
+      long = double.parse(longController.text);
+    } on FormatException {
+      return (new FormatException('Palun sisesta koordinaat komakohaga arvuna, kus koma asemel on punkt! (nt 59.3734800)')).toString();
+    } on Exception {
+      return (new Exception('Palun sisesta koordinaadid õigesti!')).toString();
+    }
+    return null;
+  }
+
+  /// Meetod implementeerib kuupäeva valimiseks kalendri
+  Future<void> _valiKuupaev(BuildContext context) async {
+    final DateTime valitud = await showDatePicker(
+        context: context,
+        initialDate: hetkeKuupaev,
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2100, 12, 31));
+    if (valitud != null && valitud != hetkeKuupaev)
+      setState(() {
+        hetkeKuupaev = valitud;
+      });
+  }
+
+  /// Meetod on popup akna kuvamiseks errori puhul
+  Future<void> _naitaErrorit(BuildContext context, String error) async {
+    return showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Viga!'),
+            content: Text(error),
+            elevation: 10,
+            actions: <Widget>[
+              TextButton(
+                child: Text('nu davaika!'),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
+          );
+        }
+    );
+  }
+
+
+  /// Siin toimub põhiline rakenduse ehitamine
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Tere! Kui tahad leida päeva pikkust,\nsiis täida järgmised väljad.',
+                  style: TextStyle(fontSize: 20)
+                ),
+                SizedBox(height: 50),
+                Text("${hetkeKuupaev.toLocal()}".split(' ')[0], style: TextStyle(fontSize: 20)),
+                //SizedBox(height: 20.0,),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(elevation: 5),
+                  onPressed: () => _valiKuupaev(context),
+                  child: Text('Vali kuupäev', style: TextStyle(fontSize: 30)),
+                ),
+                SizedBox(height: 30),
+                Text('Sisesta koordinaadid:', style: TextStyle(fontSize: 20)),
+                SizedBox(height: 10),
+                Container(
+                  width: 200,
+                  child: TextField(
+                    controller: latController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'laiuskraad'
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  width: 200,
+                  child: TextField(
+                    controller: longController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'pikkuskraad'
+                    ),
+                  ),
+                ),
+                SizedBox(height: 50),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(elevation: 5),
+                  onPressed: () {
+                    if (_kontrollKoordinaate() != null) _naitaErrorit(context, _kontrollKoordinaate());
+                    else _leiaPaevaPikkus();
+                  },
+                  child: Text('Leia päeva pikkus!', style: TextStyle(fontSize: 30))
+                ),
+                SizedBox(height: 30),
+                Card(
+                  color: Colors.white,
+                  elevation: 10,
+                  child: Container(
+                    width: 263,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text('Päiksetõus: ' + paiksetous, style: TextStyle(fontSize: 20)),
+                          Text('Päikseloojang: ' + paikseloojang, style: TextStyle(fontSize: 20)),
+                          Text('Päeva pikkus: ' + paevapikkus, style: TextStyle(fontSize: 20)),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Siia tuleb kaart'),
+              ]
             ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+
